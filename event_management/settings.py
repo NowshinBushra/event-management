65130a0cbdc5e7f88a4e280149e079da4fac878e
Nowshin Islam Bushra <nowshin.bushra399@gmail.com>
2025-02-05 17:52:06 +0600

    ready for deployment

--- event_management/settings.py
+++ event_management/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-*lp(o10kt!il)+l0#-#ix@-1i80orpbn0@ph&znj3jzc-48m))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
