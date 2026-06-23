53705854beced1f6b01d2fb5486b5332c8e41bb1
NowshinBushra <nowshin.bushra399@gmail.com>
2026-06-23 21:15:28 +0600

    Database new migration

--- event_management/settings.py
+++ event_management/settings.py
@@ -96,25 +96,25 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 
 
 # for render
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://event_management_db_am3m_user:lyv2KbYrMhmWPGTprQAkw4UYfNMksefk@dpg-d256vondiees739vtmo0-a.oregon-postgres.render.com/event_management_db_am3m',
-        conn_max_age=600
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://event_management_db_am3m_user:lyv2KbYrMhmWPGTprQAkw4UYfNMksefk@dpg-d256vondiees739vtmo0-a.oregon-postgres.render.com/event_management_db_am3m',
+#         conn_max_age=600
+#     )
+# }
 
 # For Postgres 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME', default=''),
-#         'USER': config('DB_USER', default=''),
-#         'PASSWORD': config('DB_PASSWORD', default=''),
-#         'HOST': config('DB_HOST', default='localhost'),
-#         'PORT': config('DB_PORT', cast=int)
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default=''),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', cast=int)
+    }
+}
 
 
 
